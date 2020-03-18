b1183917d60bf9886e86ce25914516b4c342c8df
Bruna <brunadaviesw@gmail.com>
2020-03-18 15:00:24 +0000

    Adds first version of formula interface

--- R/bartBMA.R
+++ R/bartBMA.R
@@ -2,8 +2,8 @@
 #' @title Bayesian Additive Regression Trees Using Bayesian Model Averaging (BART-BMA)
 #' 
 #' @description This is an implementation of Bayesian Additive Regression Trees \insertCite{chipman2010bart}{bartBMA} using Bayesian Model Averaging \insertCite{hernandez2018bayesian}{bartBMA}. 
-#' @param x.train Training data covariate matrix
-#' @param y.train Training data outcome vector.
+#' @param formula Object of class \code{formula describing the model equation
+#' @param data Training data of class, in a rectangular format (\code{data.frame}, \code{matrix}, etc). 
 #' @param a This is a parameter that influences the variance of terminal node parameter values. Default value a=3.
 #' @param nu This is a hyperparameter in the distribution of the variance of the error term. THe inverse of the variance is distributed as Gamma (nu/2, nu*lambda/2). Default value nu=3.
 #' @param sigquant Calibration quantile for the inverse chi-squared prior on the variance of the error term.
@@ -14,12 +14,13 @@
 #' @param num_rounds Number of trees. (Maximum number of trees in a sum-of-tree model). Default num_rounds=5.
 #' @param alpha Parameter in prior probability of tree node splitting. Default alpha=0.95
 #' @param beta Parameter in prior probability of tree node splitting. Default beta=1
-#' @param split_rule_node Binary variable. If equals 1, then find a new set of potential splitting points via a changepoint algorithm after adding each split to a tree. If equals zero, use the same set of potential split points for all splits in a tree. Default split_rule_node=0.
-#' @param gridpoint Binary variable. If equals 1, then a grid search changepoint detection algorithm will be used. If equals 0, then the Pruned Exact Linear Time (PELT) changepoint detection algorithm will be used (Killick et al. 2012). Default gridpoint=0.
+#' @param split_rule_node logical. If `TRUE`, then find a new set of potential splitting points via a changepoint algorithm after adding each split to a tree. If `FALSE`, use the same set of potential split points for all splits in a tree. Default split_rule_node= `TRUE`.
+#' @param cp_method Sets the rule finding method as grid search
+#' ("gridpoint") or the Pruned Exact Linear time ("PELT"). 
 #' @param maxOWsize Maximum number of models to keep in Occam's window. Default maxOWsize=100.
 #' @param num_splits Maximum number of splits in a tree
-#' @param gridsize This integer determines the size of the grid across which to search if gridpoint=1 when finding changepoints for constructing trees.
-#' @param zero_split Binary variable. If equals 1, then zero split trees can be included in a sum-of-trees model. If equals zero, then only trees with at least one split can be included in a sum-of-trees model.
+#' @param gridsize This integer determines the size of the grid across which to search if cp_method = "gridpoint" when finding changepoints for constructing trees.
+#' @param zero_split logical. If `TRUE`, then zero split trees can be included in a sum-of-trees model. If `FALSE`, then only trees with at least one split can be included in a sum-of-trees model.
 #' @param only_max_num_trees Binary variable. If equals 1, then only sum-of-trees models containing the maximum number of trees, num_rounds, are selected. If equals 0, then sum-of-trees models containing less than num_rounds trees can be selected. The default is only_max_num_trees=1.
 #' @param min_num_obs_for_split This integer determines the minimum number of observations in a (parent) tree node for the algorithm to consider potential splits of the node.
 #' @param min_num_obs_after_split This integer determines the minimum number of observations in a child node resulting from a split in order for a split to occur. If the left or right chikd node has less than this number of observations, then the split can not occur.
@@ -48,7 +49,7 @@
 #' \item{y_minmax}{Range of the input training data outcome vector.}
 #' \item{response}{Input taining data outcome vector.}
 #' \item{nrowTrain}{number of observations in the input training data.}
-#' \item{sigma}{sd(y.train)/(max(y.train)-min(y.train))}
+#' \item{sigma}{sd(y)/(max(y)-min(y))}
 #' \item{a}{input parameter}
 #' \item{nu}{input parameter}
 #' \item{lambda}{parameter determined by the inputs sigma, sigquant, and nu}
@@ -69,85 +70,169 @@
 #' xcovtest <- matrix(runif(N*p), nrow=N)
 #' ytest <- sin(pi*xcovtest[,1]*xcovtest[,2]) + 20*(xcovtest[,3]-0.5)^2+10*xcovtest[,4]+
 #' 5*xcovtest[,5]+epsilontest
-#' bart_bma_example <- bartBMA(x.train = xcov,y.train=y,x.test=xcovtest,zero_split = 1, 
+#' bart_bma_example <- bartBMA(x = xcov,y=y,x.test=xcovtest,zero_split = 1, 
 #' only_max_num_trees = 1,split_rule_node = 0)
 
 
-bartBMA<-function(x.train,...)UseMethod("bartBMA")
+bartBMA<-function(x,...)UseMethod("bartBMA")
 
 #' @rdname bartBMA
 #' @export bartBMA.default
 #' @export
-bartBMA.default<-function(x.train,y.train,
-                          a=3,nu=3,sigquant=0.9,c=1000,
-                          pen=12,num_cp=20,x.test=matrix(0.0,0,0),
-                          num_rounds=5,alpha=0.95,beta=2,split_rule_node=0,
-                          gridpoint=0,maxOWsize=100,num_splits=5,
-                          gridsize=10,zero_split=1,only_max_num_trees=1,
-                          min_num_obs_for_split=2, min_num_obs_after_split=2,
-                          exact_residuals=1,
-                          spike_tree=0, s_t_hyperprior=1, p_s_t=0.5, a_s_t=1,b_s_t=3,
-                          lambda_poisson=10,less_greedy=0,...){
+bartBMA.default<-function(formula, 
+                          data,
+                          a = 3, nu = 3,
+                          sigquant = 0.9,
+                          c = 1000,
+                          pen = 12,
+                          num_cp = 20,
+                          x.test = matrix(0.0,0,0),
+                          num_rounds = 5,
+                          alpha = 0.95, beta = 2, 
+                          split_rule_node = FALSE,
+                          cp_method = "PELT",
+                          maxOWsize = 100,
+                          num_splits = 5,
+                          gridsize = 10,
+                          zero_split = TRUE,
+                          only_max_num_trees = 1,
+                          min_num_obs_for_split = 2, 
+                          min_num_obs_after_split = 2,
+                          exact_residuals = 1,
+                          spike_tree = 0, 
+                          s_t_hyperprior = 1, 
+                          p_s_t = 0.5, a_s_t = 1,b_s_t = 3,
+                          lambda_poisson = 10,less_greedy = 0,
+                          x = NULL, y = NULL, 
+                          ...){
+  
+  #---------------------------------------------------------------------
+  ## Formula interface: extracting response and covariates 
+  if (is.null(formula)) {
+    if (is.null(y) | is.null(x)) {
+      stop("Error: Please provide a formula or an x matrix and a y vector.")
+    } 
+  } else {
+    formula <- formula(formula)
+    if (!inherits(formula, "formula")) {
+      stop("Error: Invalid formula.")
+    }
+    f <- as.formula(formula)
+    t <- terms(f, data = data)
+    
+    ## Get dependent var
+    response <- data.frame(eval(f[[2]], envir = data, enclos = parent.frame()))
+    colnames(response) <- deparse(f[[2]])
+    
+    ## Get independent vars (disconsidering interactions for now)
+    independent_vars <- attr(t, "term.labels")
+    interaction_idx <- grepl(":", independent_vars)
+    
+    main_vars <- independent_vars[!interaction_idx]
+    dat_main <- data[, main_vars, drop = FALSE]
+    
+    data.selected <- data.frame(response, 
+                                dat_main, check.names = FALSE)
+    
+    y <- data.selected[, 1]
+    # This is not dealing with factor covariates yet (!!!)
+    x <- as.matrix(data.selected[, -1, drop = FALSE])
+  }
+  
+  #---------------------------------------------------------------------
+  # Parameter definitions
   
   
-  num_obs=nrow(x.train)
-  num_vars=ncol(x.train)
+  split_rule_node <- 1 * split_rule_node
+  zero_split <- 1 * zero_split
   
-  binary=FALSE
-  start_mean=0
-  start_sd=1
-  mu=0
-  sigma_mu=0
-  sigma=sd(y.train)/(max(y.train)-min(y.train))
-  qchi = qchisq(1.0-sigquant,nu,1,0)
-  lambda = (sigma*sigma*qchi)/nu
-  if(is.factor(y.train)) {
-    # if(length(levels(y.train)) != 2) stop("y.train is a factor with number of levels != 2")
+  # Change point method 
+  gridpoint <- ifelse(cp_method == "PELT", 0, 1)
+
+  num_obs = nrow(x)
+  num_vars = ncol(x)
+  
+  # Priors'hyperparameters
+  binary = FALSE
+  start_mean = 0
+  start_sd = 1
+  mu = 0
+  sigma_mu = 0
+  sigma = sd(y)/(max(y)-min(y))
+  qchi = qchisq(1.0 - sigquant, nu, 1, 0)
+  lambda = (sigma * sigma * qchi) / nu
+  
+  
+  #---------------------------------------------------------------------
+  # Standard checkings on the input data 
+  
+  # 1. Factors are transformed into numeric?
+  if(is.factor(y)) {
+    # if(length(levels(y)) != 2) stop("y is a factor with number of levels != 2")
     binary = TRUE
-    #  y.train = as.numeric(y.train)-1
+    #  y = as.numeric(y)-1
     stop("Response must be a numeric vector")
   } else {
-    if((length(unique(y.train)) == 2) & (max(y.train) == 1) & (min(y.train) == 0)) {
+    if((length(unique(y)) == 2) & (max(y) == 1) & (min(y) == 0)) {
       warning('NOTE: assumming numeric response is binary\n')
       binary = TRUE
       #stop("Response must be a numeric vector")
     }
   }
-  if(gridsize<1) stop("gridsize must be a positive integer")
+  if(gridsize < 1) stop("gridsize must be a positive integer")
   
-  if(is.vector(x.train) | is.factor(x.train)| is.data.frame(x.train)) x.train = as.matrix(x.train)
-  if(is.vector(x.test) | is.factor(x.test)| is.data.frame(x.test)) x.test = as.matrix(x.test)
+  # 3. Always have a matrix of predictors 
+  if(is.vector(x) | is.factor(x)| is.data.frame(x)) x = as.matrix(x)
   
-  if(is.matrix(x.train)) {
+  #if(is.vector(x.test) | is.factor(x.test)| is.data.frame(x.test)) x.test = as.matrix(x.test)
+  
+  if(is.matrix(x)) {
     if(nrow(x.test)>0) {
       if(!is.matrix(x.test)) stop('x.test must be a matrix')
     } 
   }
   # check input arguments:
-  # if((!is.matrix(x.train)) || (typeof(x.train)!="double")) stop("argument x.train must be a double matrix")
+  # if((!is.matrix(x)) || (typeof(x)!="double")) stop("argument x must be a double matrix")
   # if((!is.matrix(x.test)) || (typeof(x.test)!="double")) stop("argument x.test must be a double matrix")
-  if((!is.matrix(x.train))) stop("argument x.train must be a double matrix")
+  if((!is.matrix(x))) stop("argument x must be a double matrix")
   if((!is.matrix(x.test)) ) stop("argument x.test must be a double matrix")
   if(!binary) {
-    if((!is.vector(y.train))) stop("argument y.train must be a double vector")
+    if((!is.vector(y))) stop("argument y must be a double vector")
   }
-  if(nrow(x.train) != length(y.train)) stop("number of rows in x.train must equal length of y.train")
-  if((nrow(x.test) >0) && (ncol(x.test)!=ncol(x.train))) stop("input x.test must have the same number of columns as x.train")
+  
+  # 2. Checking that the number of observations for the 
+  # predictors is the same as for the response variables
+  
+  
+  if(nrow(x) != length(y)) stop("number of rows in x must equal length of y")
+  if((nrow(x.test) >0) && (ncol(x.test)!=ncol(x))) stop("input x.test must have the same number of columns as x")
   #if((!is.na(sigest)) && (typeof(sigest)!="double")) stop("input sigest must be double")
   #if((!is.na(sigest)) && (sigest<0.0)) stop("input sigest must be positive")
   #if((mode(sigdf)!="numeric") || (sigdf<0)) stop("input sigdf must be a positive number")
-  if(c<1)stop("Value of Occam's Window has to be greater than 0."); 
-  if(num_cp<0 || num_cp>100)stop("Value of num_cp should be a value between 1 and 100."); 
   
+  # 4. Parameter checkings 
+  if(c < 1) {
+    stop("Value of Occam's Window has to be greater than 0.")
+  }
   
-  bartBMA_call=BART_BMA_sumLikelihood(less_greedy,
-                                      spike_tree, s_t_hyperprior, p_s_t, a_s_t,b_s_t,
-                                      num_obs,num_vars,lambda_poisson,
-                                      x.train,y.train,start_mean,start_sd,a,mu,nu,lambda,c,sigma_mu,
-                                      pen,num_cp,x.test,num_rounds,alpha,beta,split_rule_node,
-                                      gridpoint,maxOWsize,num_splits,gridsize,zero_split,only_max_num_trees,
-                                      min_num_obs_for_split, min_num_obs_after_split,
-                                      exact_residuals)
+  if(num_cp < 0 || num_cp > 100){
+    stop("Value of num_cp should be a value between 1 and 100.")
+  }
+  
+  #---------------------------------------------------------------------
+  # Calling main function
+  
+  bartBMA_call = BART_BMA_sumLikelihood(
+    less_greedy,
+    spike_tree, 
+    s_t_hyperprior, 
+    p_s_t, a_s_t, b_s_t,
+    num_obs, num_vars, lambda_poisson,
+    x, y, start_mean, start_sd,
+    a, mu, nu, lambda, c, sigma_mu,
+    pen, num_cp, x.test, num_rounds, alpha, beta, split_rule_node,
+    gridpoint, maxOWsize, num_splits, gridsize, zero_split, only_max_num_trees,
+    min_num_obs_for_split, min_num_obs_after_split, exact_residuals)
   
   if(length(bartBMA_call)==6){
     #length of bartBMA_call is 6 if test data was included in the call
@@ -159,13 +244,13 @@ bartBMA.default<-function(x.train,y.train,
     bartBMA_call[[5]]<-bartBMA_call[[5]]#[[length(bartBMA_call[[5]])]]
   }
   
-  bartBMA_call$numvars<-ncol(x.train)
+  bartBMA_call$numvars<-ncol(x)
   bartBMA_call$call<-match.call()
   bartBMA_call[[2]]<-bartBMA_call[[2]]#[[length(bartBMA_call[[2]])]]
   bartBMA_call[[3]]<-bartBMA_call[[3]]#[[length(bartBMA_call[[3]])]]
-  bartBMA_call$y_minmax<-range(y.train)
-  bartBMA_call$response<-y.train
-  bartBMA_call$nrowTrain<-nrow(x.train)
+  bartBMA_call$y_minmax<-range(y)
+  bartBMA_call$response<-y
+  bartBMA_call$nrowTrain<-nrow(x)
   bartBMA_call$sigma<-sigma
   bartBMA_call$a<-a
   bartBMA_call$nu<-nu
